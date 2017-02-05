6f0fe381ef9d7349ad1bba66ca5b3b685418850f
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2017-02-05 00:26:27 -0300

    1
    1

---
+++ __openerp__.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+{
+    'name': 'Ejemplo con Herencia - mfh',
+    'version': '1.1',
+    'author': 'Marlon Falcón Hernandez',
+    'category': 'mfh',
+    'description': """
+Ejemplo de Herencia
+==========================
+Descripción del módulo
+    * Texto 01
+    * Texto 02
+""",
+    'website': 'http://www.marlonfalcon.cl',
+    'depends': ['account', 'l10n_multilang'],
+    'data': [
+        'res_company_view.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+}
+
---
+++ res_company.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from openerp.osv import fields, osv
+from openerp.tools.translate import _
+
+class res_partner(osv.osv):
+	_name = 'res.partner'
+	_inherit = 'res.partner'
+	_columns = {
+	   'rut' : fields.char('Rut' , size=10 , help='Este es rut'),
+	   'emaildte' : fields.char('Emaildte' , size=256 , help='Este es mail para el dte'),
+	   'giro' : fields.char('Giro' , size=256 , help='Este es el Giro'),
+	}
+
+
+	_defaults = {
+       'giro' : 'Obras de Inegieria',
+	}
+
+res_partner()
